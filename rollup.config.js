008b41051c32968b7337140be5e50f7b5ce2247a
BMDINNER <berkemstdinner@outlook.com>
2026-03-02 04:47:05 -0800

    updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,70 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
-import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import packageJson from './package.json' with { type: 'json'  };
-
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: 'dist/index.js',
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
+      exports: 'named',
+      inlineDynamicImports: true
     },
     {
-      file: packageJson.module,
+      file: 'dist/index.esm.js',
       format: 'esm',
-      sourcemap: true
+      sourcemap: true,
+      exports: 'named',
+      inlineDynamicImports: true
     }
   ],
-
   plugins: [
     peerDepsExternal(),
     json(),
-    resolve(),
-    commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
+    resolve({
+      browser: true,
+      preferBuiltins: false,
       extensions: ['.js', '.jsx', '.ts', '.tsx']
     }),
-    terser()
-  ]
+    commonjs({
+      include: /node_modules/,
+      transformMixedEsModules: true,
+      ignore: ['util'] // Ignore util module
+    }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      rootDir: 'src'
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/preset-env', { 
+          targets: { browsers: 'last 2 versions' },
+          modules: false 
+        }],
+        '@babel/preset-react',
+        '@babel/preset-typescript'
+      ]
+    }),
+    terser({
+      format: {
+        comments: false
+      },
+      compress: {
+        drop_console: true,
+        pure_funcs: ['console.log']
+      }
+    })
+  ],
+  external: ['react', 'react-dom', 'axios'] 
 };
